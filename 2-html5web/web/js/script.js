4c04b5a16d14afe2ef906aa3ee74226a90dff1d5
ALICE5555555 <alicewen25@gmail.com>
2024-08-13 13:17:21 +0800

    0812

--- 2-html5web/web/js/script.js
+++ 2-html5web/web/js/script.js
@@ -1,6 +1,23 @@
 // 移除#about的背景影片
-$(function(){
-    if($(window).width()<821){
+$(function () {  /*document ready一個js檔只要執行一次就可以*/
+
+    //移除行動裝置的背景影片
+    if ($(window).width() < 821) {
         $('#about video').remove();
     }
+
+    //交替漢堡按鈕
+    $('.hamburger').click(function () {
+        //hamburger 交替切換 .is-active
+        $(this).toggleClass('is-active');
+        //.navigation 交替切換 .show
+        $('.navigation').toggleClass('show');
+    });
+
+    //滑動自指定位置
+    $('.menu a').click(function(){
+        let btn = $(this).attr('href');
+        let pos= $(btn).offset();
+        $('html,body').animate({scrollTop:pos.top},1000);
+    });
 });
